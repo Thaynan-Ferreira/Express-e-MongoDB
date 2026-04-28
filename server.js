5a6539f01d7ff25e6082370c2c99ee87532d4867
Thaynan-Ferreira <thaynan_ferreira@outlook.com.br>
2026-04-28 20:45:08 -0300

    Feat: Cria as primeiras rotas

--- server.js
+++ server.js
@@ -2,9 +2,15 @@ import http from "http";
 
 const PORT = 3000; // Porta onde o servidor irá escutar as requisições
 
+const rotas = {
+    "/": "Bem-vindo à página inicial!",
+    "/livros": "Entrei na rota Livros.",
+    "/autores": "Entre na rota Autores!"
+}
+
 const server = http.createServer((req, res) => {
     res.writeHead(200, { "Content-Type": "text/plain" })
-    res.end("Curso de Node.js")
+    res.end(rotas[req.url] || "Página não encontrada!")
 });
 
 server.listen(PORT, () => {
