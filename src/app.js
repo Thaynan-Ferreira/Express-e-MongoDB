15ae9f4884fb0b1cb65dddc4e471ecc42d43ecfa
Thaynan-Ferreira <thaynan_ferreira@outlook.com.br>
2026-05-01 08:38:37 -0300

    Feat: Adiciona a rota de livro. utilizando array como simulador de BD

--- src/app.js
+++ src/app.js
@@ -2,9 +2,21 @@ import express from 'express';
 
 const app = express(); // Criação da aplicação Express
 
+//array de objetos de livros para simular um banco de dados
+const livros = [
+    { id: 1, titulo: 'O Senhor dos Anéis', autor: 'J.R.R. Tolkien' },
+    { id: 2, titulo: 'Harry Potter e a Pedra Filosofal', autor: 'J.K. Rowling' },
+    { id: 3, titulo: 'O Código Da Vinci', autor: 'Dan Brown' }
+];
+
 // Rota para a página inicial
 app.get('/', (req, res) => {
     res.status(200).send('Bem-vindo à página inicial!');
-})
+});
+
+// Rota para listar todos os livros
+app.get('/livros', (req, res) => {
+    res.status(200).json(livros);
+});
 
 export default app; // Exportação da aplicação para ser utilizada em outros arquivos
