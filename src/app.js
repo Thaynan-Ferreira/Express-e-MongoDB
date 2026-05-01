e874abd09952ce4ef0aa28ca3682e6cc9fe9c5a0
Thaynan-Ferreira <thaynan_ferreira@outlook.com.br>
2026-05-01 09:41:37 -0300

    Feat: Cria a rota que busca um unico registro e atualiza esse registro

--- src/app.js
+++ src/app.js
@@ -10,6 +10,12 @@ const livros = [
     { id: 3, titulo: 'O Código Da Vinci', autor: 'Dan Brown' }
 ];
 
+function getLivroById(id) {
+    return livros.findIndex(livro => {
+        return livro.id === Number(id);
+    })
+}
+
 // Rota para a página inicial
 app.get('/', (req, res) => {
     res.status(200).send('Bem-vindo à página inicial!');
@@ -20,9 +26,36 @@ app.get('/livros', (req, res) => {
     res.status(200).json(livros);
 });
 
+// Rota para obter um livro específico
+app.get('/livros/:id', (req, res) => {
+    const livroIndex = getLivroById(req.params.id);
+
+    // Verifica se o livro existe antes de tentar retornar-lo
+    if (livroIndex !== -1) {
+        res.status(200).json(livros[livroIndex]);
+    } else {
+        res.status(404).send('Livro não encontrado!');
+    }
+})
+
+// Rota para adicionar um novo livro
 app.post('/livros', (req, res) => {
     livros.push(req.body);
     res.status(201).send('Livro adicionado com sucesso!');
+});
+
+// Rota para atualizar um livro existente
+app.put('/livros/:id', (req, res) => {
+    const livroIndex = getLivroById(req.params.id);
+
+        // Verifica se o livro existe antes de tentar atualizar-lo
+        if (livroIndex !== -1) {
+        livros[livroIndex].titulo = req.body.titulo;
+        livros[livroIndex].autor = req.body.autor;
+        res.status(200).json(livros);
+    } else {
+        res.status(404).send('Livro não encontrado!');
+    }
 });
 
 export default app; // Exportação da aplicação para ser utilizada em outros arquivos
