6d0775328f66040515c075943c42cb81cb176e79
Thaynan-Ferreira <thaynan_ferreira@outlook.com.br>
2026-05-21 21:30:20 -0300

    Refactor: refina o validador global

--- src/models/validadorGlobal.js
+++ src/models/validadorGlobal.js
@@ -2,6 +2,6 @@ import mongoose from 'mongoose';
 
 // Validação global para campos do tipo String, garantindo que não sejam vazios
 mongoose.Schema.Types.String.set("validate", {
-    validator: (valor) => valor !== "",
+    validator: (valor) => valor.trim() !== "",
     message: "O campo '{PATH}' é obrigatório" // Define a mensagem de erro personalizada para campos do tipo String que não podem ser vazios
 })
