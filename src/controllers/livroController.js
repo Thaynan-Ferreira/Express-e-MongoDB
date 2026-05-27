90a31d2a7bce19638ca41ab6291b55ed79fc77c6
Thaynan-Ferreira <thaynan_ferreira@outlook.com.br>
2026-05-26 21:53:48 -0300

    Feat: Adiciona ordenação para a apresentação dos livros

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -8,15 +8,20 @@ class LivroController {
     // Método para listar todos os livros
     static listarLivros = async (req, res, next) => {
         try {
-            let { limite = 2, pagina = 1 } = req.query; // Obtém os parâmetros de limite e página a partir dos parâmetros de consulta
+            let { limite = 5, pagina = 1, ordenacao = "_id:-1" } = req.query; // Obtém os parâmetros de limite e página a partir dos parâmetros de consulta
+
+            let [campoOrdenacao, ordem] = ordenacao.split(":"); // Divide o parâmetro de ordenação em campo e ordem
 
             limite = Number(limite); // Converte o limite para um número
             pagina = Number(pagina); // Converte a página para um número
+            ordem = Number(ordem); // Converte a ordem para um número
+            campoOrdenacao = campoOrdenacao; // Obtém o campo de ordenação
 
             if (limite <= 0 || pagina <= 0) {
                 next( new RequisicaoIncorreta());
             } else {
                 const listaLivros = await livro.find()
+                    .sort({ [campoOrdenacao]: ordem }) // Ordena os livros pelo campo e ordem especificados
                     .skip((pagina - 1) * limite) // Calcula o número de documentos a pular com base na página e no limite
                     .limit(Number(limite)) // Limita o número de documentos retornados com base no limite
                     .populate("autor")
